e1f5263c66d375fea6a18a6fdc9bee3a174afeac
tfukasa <tfukasa@C010423523.zipangu.cac.co.jp>
2026-04-16 14:05:29 +0900

    Question6 完了

--- java_comprehension_exercises_volume1/src/basic/question06/Question06_2.java
+++ java_comprehension_exercises_volume1/src/basic/question06/Question06_2.java
@@ -6,7 +6,15 @@ public class Question06_2 {
 
 	public static void main(String[] args) throws IOException {
 
+		String[] studentName = { "かなこ", "ゆうた", "あい", "はるひ", "かずき" };
+
+		for (int i = 0; i < studentName.length; i++) {
+
+			System.out.println(studentName[i]);
+
+		}
+
 		// TODO:ここに実装
-		
+
 	}
 }
--- java_comprehension_exercises_volume1/src/basic/question06/Question06_3.java
+++ java_comprehension_exercises_volume1/src/basic/question06/Question06_3.java
@@ -1,13 +1,37 @@
 package basic.question06;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Question06_3 {
-	
+
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		
+
+		int amount = 0;
+		int price = 100;
+
+		System.out.println("鉛筆を購入しますか?");
+		System.out.print("はい：0、いいえ：1 >");
+
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		int buyFlag = Integer.parseInt(reader.readLine());//そのまま数値入力できる!!
+
+		while (buyFlag == 0) {
+
+			amount++;
+			System.out.println("購入する鉛筆の本数を1つ増やしますか?");
+			System.out.print("はい：0、いいえ：1 >");
+			buyFlag = Integer.parseInt(reader.readLine());
+
+		}
+
+		System.out.println("購入した鉛筆の本数は" + amount + "本です。");
+
+		System.out.println("購入した鉛筆の合計金額は" + (amount * price) + "円です。");
+
 		// TODO:ここに実装
-		
+
 	}
 
 }
