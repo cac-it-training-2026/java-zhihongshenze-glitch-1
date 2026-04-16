d106ca6205112b613e6440226c3aa8290922ecaa
tfukasa <tfukasa@C010423523.zipangu.cac.co.jp>
2026-04-16 13:50:52 +0900

    Question01 完了

--- java_comprehension_exercises_volume1/src/basic/question01/Question01_1.java
+++ java_comprehension_exercises_volume1/src/basic/question01/Question01_1.java
@@ -8,6 +8,17 @@ public class Question01_1 {
 
 	public static void main(String[] args) {
 
+		int price = 120;
+		String item = "ノート";
+
+		System.out.println(price);
+		System.out.println(item);
+
+		price = 200;
+		item = "鉛筆";
+
+		System.out.println(price);
+		System.out.println(item);
 		// TODO:ここに実装
 
 	}
--- java_comprehension_exercises_volume1/src/basic/question01/Question01_2.java
+++ java_comprehension_exercises_volume1/src/basic/question01/Question01_2.java
@@ -7,7 +7,17 @@ package basic.question01;
 public class Question01_2 {
 
 	public static void main(String[] args) {
-		
+
+		int applePrice = 100;
+		String stationery = "鉛筆";
+
+		int bananaPrice = applePrice;
+		String writingUtensils = stationery;
+
+		System.out.println("りんごの値段は" + applePrice + "円です。");
+		System.out.println("バナナの値段は" + bananaPrice + "円です。");
+		System.out.println("この筆記用具は" + writingUtensils + "です。");
+
 		// TODO:ここに実装
 
 	}
