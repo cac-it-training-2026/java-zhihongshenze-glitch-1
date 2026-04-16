b936e8c351e8d4c84a01282116388d22ebd12b6b
tfukasa <tfukasa@C010423523.zipangu.cac.co.jp>
2026-04-16 13:55:26 +0900

    Question3 完了

--- java_comprehension_exercises_volume1/src/basic/question03/Question03_1.java
+++ java_comprehension_exercises_volume1/src/basic/question03/Question03_1.java
@@ -8,7 +8,14 @@ public class Question03_1 {
 
 	public static void main(String[] args) {
 
+		int price = 30;
+		int totalBudget = 300;
+
+		int quantity = (totalBudget / price);
+
+		System.out.println("商品の購入数は" + quantity + "個です。");
+
 		// TODO:ここに実装
-		
+
 	}
 }
--- java_comprehension_exercises_volume1/src/basic/question03/Question03_2.java
+++ java_comprehension_exercises_volume1/src/basic/question03/Question03_2.java
@@ -8,7 +8,23 @@ public class Question03_2 {
 
 	public static void main(String[] args) {
 
+		int basePrice = 1200;
+		int tickets = 3;
+
+		int reducedPrice = (basePrice - 200);
+		int reducedTickets = (--tickets);//前置デクリメント
+
+		int totalPrice = (reducedPrice * reducedTickets);
+
+		double taxRate = 1.1;
+		int totalWithTax = (int) (totalPrice * taxRate);
+
+		System.out.println("購入可能枚数は" + reducedTickets + "枚です。");
+		System.out.println("値下げの1枚あたりの料金は" + reducedPrice + "円です。");
+		System.out.println("合計金額 (税抜) は" + totalPrice + "です。");
+		System.out.println("合計金額 (税込) は" + totalWithTax + "です。");
+
 		// TODO:ここに実装
-		
+
 	}
 }
