4f5b34f0e87ed46b54d5de633dffce45fd515ef1
tfukasa <tfukasa@C010423523.zipangu.cac.co.jp>
2026-04-16 13:58:41 +0900

    Question4 完了

--- java_comprehension_exercises_volume1/src/basic/question04/Question04_1.java
+++ java_comprehension_exercises_volume1/src/basic/question04/Question04_1.java
@@ -10,6 +10,15 @@ public class Question04_1 {
 
 	public static void main(String[] args) throws IOException {
 
+		int[] ageGroup = new int[5];
+
+		ageGroup[0] = 12;
+		ageGroup[1] = 18;
+		ageGroup[2] = 24;
+		ageGroup[3] = 19;
+		ageGroup[4] = 20;
+
+		System.out.println("3番目の人の年齢は" + ageGroup[2] + "歳です。");
 		// TODO:ここに実装
 
 	}
--- java_comprehension_exercises_volume1/src/basic/question04/Question04_2.java
+++ java_comprehension_exercises_volume1/src/basic/question04/Question04_2.java
@@ -1,6 +1,8 @@
 package basic.question04;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * 第7章 配列
@@ -10,8 +12,20 @@ public class Question04_2 {
 
 	public static void main(String[] args) throws IOException {
 
+		int[] ageGroup = { 23, 33, 26, 21, 25, 22 };
+
+		System.out.println("4番目の人の年齢を入力してください。");
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		String str = reader.readLine();
+		int temporaryAge = Integer.parseInt(str);
+
+		ageGroup[3] = temporaryAge;
+
+		System.out.println("4番目の人の年齢は" + ageGroup[3] + "歳です。");
+		System.out.println("人数は" + ageGroup.length + "人です。");
+
 		// TODO:ここに実装
-		
+
 	}
 
 }
